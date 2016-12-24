5f8f51959f5174815fafa8a2fc62fcee8c07a2f2
Lovish Dogra <lovishdogra7@gmail.com>
2016-12-24 15:54:10 +0530

    Icon implementation

--- CreativeAppDirectorTeacher/CreativeAppDirectorTeacher/TeacherHomeViewC.swift
+++ CreativeAppDirectorTeacher/CreativeAppDirectorTeacher/TeacherHomeViewC.swift
@@ -9,27 +9,67 @@
 import UIKit
 
 class TeacherHomeViewC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: Declaration & IBOutlets
+        //Views
+    @IBOutlet weak var viewTopMasterContainer: UIView!
+    @IBOutlet weak var viewTopbarContainer: UIView!
+    @IBOutlet weak var collectionViewMenubar: UICollectionView!
+    @IBOutlet weak var viewSubClassLogoContainer: UIView!
+    @IBOutlet weak var collectionViewStudentsList: UICollectionView!
+    @IBOutlet weak var viewContainerView: UIView!
+    
+        //Buttons
+    @IBOutlet weak var btnLogout: UIButton!
+    @IBOutlet weak var btnSelectAll: UIButton!
+    
+        //Labels
+    @IBOutlet weak var labelDate: UILabel!
+    @IBOutlet weak var labelLogout: UILabel!
+    @IBOutlet weak var labelWelcomeTeacher: UILabel!
+    @IBOutlet weak var labelClassName: UILabel!
+    @IBOutlet weak var labelSelectAll: UILabel!
+    
+        //Images
+    @IBOutlet weak var imageViewLogo: UIImageView!
+    
+    //MARK: IBActions
+    
+    @IBAction func tapLogout(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func tapSelectAll(_ sender: Any) {
     }
-    */
-
+    
+    //MARK: Viewlife methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialization()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //MARK: Private Methods
+    func initialization(){
+        
+        //Icons
+        btnLogout.setTitle("fa:sign-out", for: .normal)
+        btnLogout.parseIcon()
+        btnSelectAll.setTitle("fa:users", for: .normal)
+        btnSelectAll.parseIcon()
+        
+        
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
